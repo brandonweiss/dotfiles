fc1707e05f1fc37ca17a9b240f11bad2b54e160c
Brandon Weiss <brandon@anti-pattern.com>
2014-04-09 22:43:06 -0700

    Making the name argument of the download function optional

--- fish/functions/download.fish
+++ fish/functions/download.fish
@@ -1,11 +1,19 @@
 function download
-  set url      $argv[1]
-  set filename $argv[2]
-  set file     ~/Downloads/$filename
+  set url $argv[1]
+
+  if test (count $argv) -eq 2
+    set filename $argv[2]
+  else
+    set filename (echo (basename $url) | sed 's/?.*//g')
+  end
+
+  set file ~/Downloads/$filename
 
   if test -e $file
-    wget --continue --output-document=$file $url
-  else
-    wget --output-document=$file $url
+    set arguments "--continue"
   end
+
+  set arguments $arguments --output-document=$file
+
+  wget $arguments $url
 end
