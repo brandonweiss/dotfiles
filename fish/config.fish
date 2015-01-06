8a2772b33b2a5b7706f5badd169f0cd78f8e48c9
Brandon Weiss <brandon@anti-pattern.com>
2015-01-06 01:14:02 -0800

    rbenv now has built-in support for fish

--- fish/config.fish
+++ fish/config.fish
@@ -1,13 +1,11 @@
 set -g fish_greeting ""
 
+status --is-interactive; and . (rbenv init -|psub)
+
 set PATH "/usr/local/share/npm/bin" $PATH # npm
 set PATH "./node_modules/.bin"      $PATH # local npm
-set PATH "$HOME/.rbenv/bin"         $PATH # rbenv
-set PATH "$HOME/.rbenv/shims"       $PATH # rbenv
 set PATH "/usr/local/heroku/bin"    $PATH # Heroku Toolbelt
 set PATH "./bin"                    $PATH # Project-specific binstubs
-
-rbenv rehash >/dev/null ^&1
 
 set -x EDITOR     "atom --wait"
 set -x GIT_EDITOR "atom --wait"
