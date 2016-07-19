cd9cbd218e7fcc2fbb5da28c9df9883b244da84c
Brandon Weiss <brandon@anti-pattern.com>
2016-07-19 07:21:44 -0700

    Removing path to old npm

--- fish/config.fish
+++ fish/config.fish
@@ -5,10 +5,9 @@ status --is-interactive; and . (rbenv init -|psub)
 set GIT_CONTRIB (brew --prefix)"/share/git-core/contrib"
 set DIFF_HIGHLIGHT "$GIT_CONTRIB/diff-highlight"
 
-set PATH "/usr/local/share/npm/bin" $PATH # npm
-set PATH "./node_modules/.bin"      $PATH # local npm
-set PATH $DIFF_HIGHLIGHT            $PATH # diff-so-fancy
-set PATH "./bin"                    $PATH # Project-specific binstubs
+set PATH "./node_modules/.bin" $PATH # local npm
+set PATH $DIFF_HIGHLIGHT       $PATH # diff-so-fancy
+set PATH "./bin"               $PATH # Project-specific binstubs
 
 bass source ~/.nvm/nvm.sh ';' nvm > /dev/null
 
