cc42a5ba9ed897fa818073a61c5a5dbc13dc4536
Brandon Weiss <brandon@anti-pattern.com>
2017-09-29 16:51:25 -0700

    Add Droid Sans Mono to scripts/install_fonts
    This is for my terminal font.

--- scripts/install_fonts.sh
+++ scripts/install_fonts.sh
@@ -1,2 +1,5 @@
 # Install San Francisco Monospace fonts for editor
 cp /Applications/Utilities/Terminal.app/Contents/Resources/Fonts/* ~/Library/Fonts
+
+# Install other fonts
+cp fonts/* ~/Library/Fonts
