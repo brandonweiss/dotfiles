b1bd1292dd0be9cdb10a62899bb2025066c8c0a4
Brandon Weiss <brandon@anti-pattern.com>
2023-05-31 08:07:14 -0700

    Fix paths and add aliases

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,12 +1,13 @@
 set fish_greeting # Disable Fish greeting
 
-source (brew --prefix asdf)"/asdf.fish"
-source (brew --prefix autojump)"/share/autojump/autojump.fish"
+set PATH /opt/homebrew/bin $PATH
 
-set dotfiles_directory (dirname (dirname (dirname (readlink (status --filename)))))
+source (brew --prefix asdf)"/libexec/asdf.fish"
+source (brew --prefix autojump)"/share/autojump/autojump.fish"
 
 set PATH ./node_modules/.bin $PATH
 set PATH ./bin $PATH
+set dotfiles_directory (dirname (dirname (dirname (readlink (status --filename)))))
 set PATH $dotfiles_directory/bin $PATH
 
 # Variables
@@ -15,10 +16,21 @@ set --export GIT_EDITOR "code --wait"
 
 alias av "aversion ~/Code"
 alias bruo "brew update; echo ""; brew outdated"
-alias e "code"
+alias e code
+
+# Bundler
+alias bc "bundle check"
+alias be "bundle exec"
+alias bi "bundle install"
+alias bo "bundle outdated"
+alias bu "bundle update"
+
+# Rails
+alias rc "rails console"
+alias rs "rails server"
 
 starship init fish | source
 
 function rm
-  rm-ruby $argv
+    rm-ruby $argv
 end
